effa0955ae73d60e7aeb0ff3b77a5efa2b9479b7
myway00 <junior0614@naver.com>
2023-09-21 15:38:22 +0900

    fix : main content (ci cd 점검 용도)

--- src/main/java/com/study/cloud/controller/CloudController.java
+++ src/main/java/com/study/cloud/controller/CloudController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CloudController {
     @GetMapping("/")
     public String welcome() {
-        return "welcome";
+        return "contents changed ! ";
     }
 }
